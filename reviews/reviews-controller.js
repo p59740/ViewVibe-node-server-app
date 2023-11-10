2851eb02c7905b86b96d07fde208dddfa2ecb0d7
Sylvia Lu <sylvialu@Sylvias-MacBook-Air.local>
2023-11-10 15:46:44 -0800

    added reviews

---
+++ reviews/reviews-controller.js
@@ -0,0 +1,39 @@
+import * as dao from "./reviews-dao.js";
+
+const ReviewsController = (app) => {
+    const createReview = async (req, res) => {
+        const review = req.body;
+        const actualReview = await dao.createReview(review);
+        res.json(actualReview);
+    };
+
+    const findAllReviews = async (req, res) => {
+        const params = req.query;
+        let reviews = null;
+        if (params.imdbID !== undefined) {
+            reviews = await dao.findReviewByIMDBID(params.imdbID);
+        }
+        else if (params.username !== undefined) {
+            reviews = await dao.findByUserName(params.username);
+        }
+        else {
+            reviews = await dao.findAllReviews();
+        }
+        reviews.sort((r1, r2) => r2.time - r1.time);
+        res.json(reviews);
+    };
+
+    const deleteReview = async (req, res) => {
+        const rid = req.params.rid;
+        const status = await dao.deleteReview(rid);
+        res.json(status);
+    };
+
+    app.post("/review", createReview);
+
+    app.get("/review", findAllReviews);
+
+    app.delete("/review/:rid", deleteReview);
+};
+
+export default ReviewsController;
---
+++ reviews/reviews-dao.js
@@ -0,0 +1,11 @@
+import reviewsModel from "./reviews-model.js";
+
+export const createReview = (review) => reviewsModel.create(review);
+
+export const findAllReviews = () => reviewsModel.find();
+
+export const findReviewByIMDBID = (imdbID) => reviewsModel.find({ imdbID: imdbID });
+
+export const findByUserName = (username) => reviewsModel.find({ username: username });
+
+export const deleteReview = (rid) => reviewsModel.deleteOne({ _id: rid });
