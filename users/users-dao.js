6c91d115e36fd8d40f5bd2e29a9bc921ef5e15b4
p59740 <peng.yue2@northeatsern.edu>
2023-11-11 21:11:26 -0800

    users back-end

---
+++ users/users-dao.js
@@ -0,0 +1,28 @@
+import usersModel from "./users-model.js";
+
+export const register = async (user) => {
+    const existingUser = await findByUsername(user.username);
+    if (existingUser) {
+    }
+  };
+
+export const createUser = (user) => usersModel.create(user);
+
+export const findByCredentials = (username, password) =>
+  usersModel.findOne({ username, password }, { password: false });
+  
+export const findAllUsers = () => usersModel.find();
+
+export const findUserById = (uid) => usersModel.findById(uid);
+
+export const findByUsername = (username) => usersModel.findOne({ username });
+
+export const deleteUser = (uid) => usersModel.deleteOne({ _id: uid });
+
+export const updateUser = (uid, userUpdates) =>
+  usersModel.updateOne({ _id: uid }, { $set: userUpdates });
+
+export const addLike = (liked, username) => usersModel.findOneAndUpdate({username: liked}, {$push: {likes: username}});
+
+export const findUserByUsername = (username) =>
+  usersModel.findOne({ username });
---
+++ users/users.js
@@ -0,0 +1,18 @@
+export default [
+    {
+      _id: "1",
+      username: "yuepeng",
+      password: "yuepeng",
+    },
+    {
+      _id: "2",
+      username: "siyinglu",
+      password: "siyinglu",
+    },
+    {
+      _id: "3",
+      username: "bobo",
+      password: "bobo",
+    },
+  ];
+  
